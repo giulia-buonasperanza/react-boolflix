2288496636658ae4cbe0495dc8e283b40adcd82d
giulia-buonasperanza <giulia.9402@gmail.com>
2026-05-18 19:31:25 +0200

    inserisco nella Home i trending movie e series e cossa mostrare con i filtri.

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,6 +1,42 @@
+import { useContext } from "react";
+import { GlobalContext } from "../contexts/GlobalContext";
+
+
 function HomePage() {
-  return (
-    <h1>Home</h1>
+  const {
+    trendingMovies,
+    trendingSeries,
+    movies,
+    series,
+    search,
+    selectedGenre
+  } = useContext(GlobalContext);
+
+  const moviesToShow = search.trim() === "" ? trendingMovies : movies;
+  const seriesToShow = search.trim() === "" ? trendingSeries : series;
+
+  const filterByGenre = moviesToShow.filter((movie) => {
+    if (selectedGenre === "all") {
+      return true;
+    }
+    return movie.genre_ids.includes(parseInt(selectedGenre));
+  });
+
+  return (<>
+
+    <h2>Movies</h2>
+    <ul>
+      {filterByGenre.map((movie) => (
+        <li key={movie.id}>{movie.title}</li>
+      ))}
+    </ul>
+    <h2>Series</h2>
+    <ul>
+      {seriesToShow.map((serie) => (
+        <li key={serie.id}>{serie.name}</li>
+      ))}
+    </ul>
+  </>
   );
 }
 export default HomePage;
