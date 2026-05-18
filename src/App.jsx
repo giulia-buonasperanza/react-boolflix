5baa6cc06d05f644acf044c75c6b111c48a0dae9
giulia-buonasperanza <giulia.9402@gmail.com>
2026-05-18 16:25:11 +0200

    creato il contesto globale (fetch serie e film, search)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { BrowserRouter, Route, Routes } from "react-router";
-import { ThemeProvider } from "./contexts/ThemeContext";
 import MainLayout from "./layouts/MainLayout";
 import HomePage from "./pages/HomePage";
 import NotFound from "./pages/NotFound";
+import { GlobalContext } from "./contexts/GlobalContext";
 
 function App() {
   return (
-    <ThemeProvider>
+    <GlobalContext>
       <BrowserRouter>
         <Routes>
           <Route element={<MainLayout />}>
             <Route index element={<HomePage />} />
+            <Route path="/films/:id" element={<Film />} />
             <Route path="*" element={<NotFound />} />
           </Route>
         </Routes>
       </BrowserRouter>
-    </ThemeProvider>
+    </GlobalContext>
   );
 }
 export default App;
---
+++ src/contexts/GlobalContext.jsx
@@ -0,0 +1,81 @@
+import { createContext, useState } from "react";
+
+const GlobalContext = createContext();
+const API_TMDB_BASE_URL = "https://api.themoviedb.org/3";
+const API_TMDB_KEY = import.meta.env.VITE_TMDB_API_KEY;
+const API_TMDB_TOKEN = import.meta.env.VITE_TMDB_TOKEN;
+
+function GlobalProvider({ children }) {
+    const [movies, setMovies] = useState([]);
+    const [series, setSeries] = useState([]);
+    const [search, setSearch] = useState("");
+    const [isLoading, setIsLoading] = useState(false);
+
+
+
+    function fetchContents() {
+
+        const options = {
+            method: 'GET',
+            headers: {
+                accept: 'application/json',
+                Authorization: `Bearer ${API_TMDB_TOKEN}`
+            }
+        };
+
+        const movieUrl = `${API_TMDB_BASE_URL}/search/movie?query=${search}&language=it-IT`;
+        const serieTvUrl = `${API_TMDB_BASE_URL}/search/tv?query=${search}&language=it-IT`;
+
+        if (search.trim() === "") {
+            setMovies([]);
+            setSeries([]);
+            return;
+        };
+        setIsLoading(true);
+
+        const movieFetch = fetch(movieUrl, options)
+            .then((response) => {
+                return response.json();
+            });
+
+
+        const serieTvFetch = fetch(serieTvUrl, options)
+            .then((response) => {
+                return response.json();
+            });
+
+
+        Promise.all([movieFetch, serieTvFetch])
+            .then(([movieData, serieTvData]) => {
+                setMovies(movieData.results);
+                setSeries(serieTvData.results);
+            })
+            .catch((error) => {
+                console.error("Errore durante il fetch:", error);
+            })
+            .finally(() => {
+                setIsLoading(false);
+            });
+    };
+
+
+    const contextValue = {
+        movies,
+        series,
+        search,
+        setSearch,
+        isLoading,
+        fetchContents
+    };
+
+    return (
+        <GlobalContext value={contextValue}>
+            {children}
+        </GlobalContext>
+    );
+};
+
+
+
+
+export { GlobalProvider, GlobalContext };
